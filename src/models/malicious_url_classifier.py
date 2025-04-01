bf59b15bbed510c762c1207e6b409a36b4e155f2
mda1422 <maxamedmustafe2384@gmail.com>
2025-04-01 14:23:37 +0100

    refactor: update sys.path configuration and import statements in GUI files

--- src/models/malicious_url_classifier.py
+++ src/models/malicious_url_classifier.py
@@ -3,7 +3,7 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
 
 
-from sklearn.externals import joblib
+import joblib
 
 class URLClassifier:
     def __init__(self):
